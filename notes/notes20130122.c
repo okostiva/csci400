3c0866905c4342a5dcd7e2be44d49de551ef06f5
Orion Kostival <okostiva@mines.edu>
2013-01-22 09:08:26 -0700

    Notes for 1/22/2013

--- notes/notes20130122.c
+++ notes/notes20130122.c
@@ -3,5 +3,23 @@
 //
 //
 /*	
-
+   
 */
+
+int main(void)
+{
+    p = NULL;
+    bufsize = 0;
+    
+    //realloc will simply extend the block of currently allocated memory
+    //if possible, and if not, allocate a new block that is large enough, 
+    //copy the contents and release the original buffer rather than using 
+    //malloc where the memory must be freed and then allocated again
+    p = (char*)realloc(p, bufsize*sizeof(char));
+    if (!p)
+       return NULL;
+       
+    malloc(strlen(yytext)+1);
+    strcopy(temp, yytext);
+    temp = srtdup(yytext);
+}
