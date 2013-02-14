160dbd289aa4fadf942e84c38f1a8db5b98f71d4
Orion Kostival <okostiva@mines.edu>
2013-02-14 08:51:52 -0700

    Finalized project 3 and notes for 2/14

---
+++ notes/notes20130214.c
@@ -0,0 +1,43 @@
+//CSCI 400
+//Class notes 02/14/2013
+//
+//
+/*	
+
+   x;
+
+   fred
+   {
+      y;
+      y=2;
+      
+      sue
+      {
+         z;
+         y=5;
+
+         return y;         
+      }
+      
+      sue()
+      print y;
+   }
+   
+   bob
+   {
+      w;
+      
+      w=sue()
+      print w;
+   }
+   
+   (define (cl x)
+      (let* ((z 5) (y 20) (fn (lambda (x) (* x y z))))
+         (let ((y 10))
+            (fn y)
+         )
+      )
+   )
+   
+   (cl 4)
+*/
